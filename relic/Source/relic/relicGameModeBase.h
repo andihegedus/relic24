e3fa5f3ee901cdb67ba9dfe0522a69234385a0c3
andihegedus <74625948+andihegedus@users.noreply.github.com>
2024-03-28 13:54:04 -0400

    Create blank unreal project to start

---
+++ relic/Source/relic/relicGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "relicGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RELIC_API ArelicGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
